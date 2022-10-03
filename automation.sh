fe9c2b0c3122c71cdaa590747c24709b35903cf4
Prakash Dash <prakash@Prakashs-MacBook-Air.local>
2022-10-03 20:31:03 +0530

    Added code for inventory creation and cron job

--- automation.sh
+++ automation.sh
@@ -25,3 +25,34 @@ tar -cvf /tmp/$my_name-httpd-logs-$timestamp.tar /var/log/apache2/*.log
 #The script should run the AWS CLI command and copy the archive to the s3 bucket.
 aws s3 cp /tmp/$my_name-httpd-logs-$timestamp.tar s3://${s3_bucket}/${my_name}-httpd-logs-${timestamp}.tar
 
+
+#Creare inventory file
+
+LOGTYPE="Log Type"
+DATE="Date Created"
+Logtype1=$(ls -lt /tmp | grep httpd | head -1 |awk {'print $9'} | awk -F "-" {'print $2"-"$3'})
+Type=$(ls -lt /tmp | grep httpd | head -1 | awk {'print $9'} | awk -F "." {'print $2'})
+Date1=$(ls -lt /tmp | grep httpd | head -1 | awk {'print $9'} | awk -F "." {'print $1'}| awk -F "-" {'print $4"-"$5'})
+Size=$(ls -lth  /tmp | grep httpd | head -1 |awk {'print $5'})
+if [ -f "/var/www/html/inventory.html" ]
+then
+printf "<p>" >> /var/www/html/inventory.html
+printf  "$Logtype1 $Date1 $Type $Size" >> /var/www/html/inventory.html
+printf "<p>" >> /var/www/html/inventory.html
+else
+touch /var/www/html/inventory.html
+printf "%s\t" "$LOGTYPE" "$DATE"  "Type" "Size" >> /var/www/html/inventory.html
+printf "<p>" >> /var/www/html/inventory.html
+printf  "$Logtype1 $Date1 $Type $Size" >> /var/www/html/inventory.html
+printf "<p>" >> /var/www/html/inventory.html
+fi
+
+#Add the cron job
+
+if [ -f "/etc/cron.d/automation" ];
+then
+        echo "Automation script in place for Daily 00:00 hrs"
+else
+        touch /etc/cron.d/automation
+        printf "0 0 * * * root /root/Automation_Project/auotmation.sh" > /etc/cron.d/automation
+fi
